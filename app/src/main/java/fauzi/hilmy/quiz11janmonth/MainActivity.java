ca0e14b6c18bd437a535e4f9f02293857e95f1ac
Fauzee26 <32213481+Fauzee26@users.noreply.github.com>
2018-01-15 14:54:34 +0700

    Last

--- app/src/main/java/fauzi/hilmy/quiz11janmonth/MainActivity.java
+++ app/src/main/java/fauzi/hilmy/quiz11janmonth/MainActivity.java
@@ -26,49 +26,33 @@ public class MainActivity extends AppCompatActivity {
         btnResult.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //mengambil nilai dari widget editText dan memasukkan ke nilai string
                 String nMonth = etMonth.getText().toString();
 
-                //mengecek apakah editText kosong
-                //kondisi ketika panjangnya kosong
                 if (nMonth.equalsIgnoreCase("Januari" )) {
-                    //memberi warning berupa error
                     lblResult.setText("This Is First Month");
                 }else if (nMonth.equalsIgnoreCase("Februari" )) {
-                    //memberi warning berupa error
                     lblResult.setText("This Is Second Month");
                 }else if (nMonth.equalsIgnoreCase("Maret" )) {
-                    //memberi warning berupa error
                     lblResult.setText("This Is Third Month");
                 }else if (nMonth.equalsIgnoreCase("April" )) {
-                    //memberi warning berupa error
                     lblResult.setText("This Is Fourth Month");
                 }else if (nMonth.equalsIgnoreCase("May" )) {
-                    //memberi warning berupa error
                     lblResult.setText("This Is Fifth Month");
                 }else if (nMonth.equalsIgnoreCase("June" )) {
-                    //memberi warning berupa error
                     lblResult.setText("This Is Sixth Month");
                 }else if (nMonth.equalsIgnoreCase("July" )) {
-                    //memberi warning berupa error
                     lblResult.setText("This Is Seventh Month");
                 }else if (nMonth.equalsIgnoreCase("August" )) {
-                    //memberi warning berupa error
                     lblResult.setText("This Is Eighth Month");
                 }else if (nMonth.equalsIgnoreCase("September" )) {
-                    //memberi warning berupa error
                     lblResult.setText("This Is Ninth Month");
                 }else if (nMonth.equalsIgnoreCase("Oktober" )) {
-                    //memberi warning berupa error
                     lblResult.setText("This Is Tenth Month");
                 }else if (nMonth.equalsIgnoreCase("November" )) {
-                    //memberi warning berupa error
                     lblResult.setText("This Is Eleventh Month");
                 }else if (nMonth.equalsIgnoreCase("Desember" )) {
-                    //memberi warning berupa error
                     lblResult.setText("This Is Twelveth Month");
                 }else{
-                    //mengubah nilai dari string k integer
                     etMonth.setError("Your Data Is Wrong");
                 }
             }
